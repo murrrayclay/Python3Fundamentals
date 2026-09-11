c2d7c91f2b1b087f9eb37280385cc11984b09c31
murrrayclay <32752228+murrrayclay@users.noreply.github.com>
2026-09-11 19:51:04 +1000

    prog

--- roll_dice.py
+++
@@ -1,9 +0,0 @@
-import random
-roll = random.randint(1,6)
-guess = int(input('Guess the dice roll:\n'))
-if guess == roll:
-    print("Correct! They rolled a " + str(roll))
-else:
-    print("WROOONG! They rolled a " + str(roll))
-    
-    
