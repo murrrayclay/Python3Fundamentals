92c6a80a38a05a6aeb419342a6fcee13d260ed1e
murrayclayUM890PRO <clay.murray@gmail.com>
2026-09-18 17:46:43 +1000

    progress

---
+++ 06_ParseNestedContactsDict.py
@@ -0,0 +1,19 @@
+contacts = {
+    'number': 4,
+    'students':
+    [
+        {'name':'Sarah Holderness', 'email':'sarah@example.com'},
+        {'name':'Harry Potter', 'email':'harry@example.com' },
+        {'name':'Hermione Granger', 'email':'hermione@example.com'}
+    ]
+}
+
+print('Student emails:')
+for student in contacts['students']:
+    print(student['email'])
+    
+
+
+
+
+
